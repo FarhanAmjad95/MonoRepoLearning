4b8fe824ee124a3ce91322df851afdd03481fefc
Farhan Amjad <amjad.farhan@systemsltd.com>
2023-12-18 12:54:42 +0500

    monorepo config changes

--- Package.swift
+++ Package.swift
@@ -7,18 +7,21 @@ let package = Package(
     name: "EtisalatMonoRepo",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "EtisalatMonoRepo",
-            targets: ["EtisalatMonoRepo"])
+        .library(name: "someComponent",     targets: ["someComponent"]),
+        .library(name: "anotherComponent",  targets: ["anotherComponent"]),
+        
     ],
-    dependencies: [ .package(path: "./someComponent"), .package(path: "./anotherComponent")],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "EtisalatMonoRepo", dependencies: [.product(name: "someComponent", package: "someComponent"), .product(name: "anotherComponent", package: "anotherComponent")]),
-        .testTarget(
-            name: "EtisalatMonoRepoTests",
-            dependencies: ["EtisalatMonoRepo"])
+        .target(name: "someComponent", dependencies: [],
+                path: "someComponent/Sources/someComponent"),
+        .testTarget(name: "someComponentTests", dependencies: ["someComponent"],
+                    path: "someComponent/Tests/someComponentTests"),
+        .target(name: "anotherComponent", dependencies: [],
+                path: "anotherComponent/Sources/anotherComponent"),
+        .testTarget(name: "anotherComponentTests", dependencies: ["anotherComponent"],
+                    path: "anotherComponent/Tests/anotherComponentTests"),
     ]
 )
